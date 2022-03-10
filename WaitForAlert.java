c2308872bb805b3c3573f4b144124838975dc408
Balajisridhar3108 <92989836+Balajisridhar3108@users.noreply.github.com>
2022-03-10 21:22:02 +0530

    Add files via upload

---
+++ WaitForAlert.java
@@ -0,0 +1,37 @@
+package week5.day4;
+
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WaitForAlert {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.leafground.com/home.html");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.findElement(By.xpath("//h5[text()='Wait for Alert']")).click();
+		WebElement findElement = driver.findElement(By.xpath("//button[text()='Click to get Alert']"));
+		findElement.click();
+		
+		WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		webDriverWait.until(ExpectedConditions.alertIsPresent());
+		Alert alert = driver.switchTo().alert();
+		alert.accept();
+		String text = findElement.getText();
+		System.out.println(text);
+		
+
+	}
+
+}
